d9300382dcc4c56539bec233b49a363e7df3b63f
Lohan Batista <lohan_vinicius@hotmail.com>
2021-01-06 09:34:20 -0300

    aula final

--- 5 - React - Abstraindo seu CSS com Styled Components/src/Components/GlobalStyles.js
+++ 5 - React - Abstraindo seu CSS com Styled Components/src/Components/GlobalStyles.js
@@ -8,6 +8,6 @@ font-family: "Montserrat", sans-serif;
 margin: 0;
 padding: 0;
 text-decoration: none;
-color: grey; 
+
 }
 `;
--- 5 - React - Abstraindo seu CSS com Styled Components/src/Components/UI/variaveis.js
+++ 5 - React - Abstraindo seu CSS com Styled Components/src/Components/UI/variaveis.js
@@ -1,3 +1,9 @@
 export const corPrimaria = "#7E1600";
-export const fundoClario = "";
-export const textoFundoEscuro = "";
+
+export const fundoClaro = "#f1f1f1";
+export const conteudoClaro = "white";
+export const textoFundoClaro = "gray";
+
+export const fundoEscuro = "#363537";
+export const conteudoEscuro = "#292929";
+export const textoFundoEscuro = "#FAFAFA";
