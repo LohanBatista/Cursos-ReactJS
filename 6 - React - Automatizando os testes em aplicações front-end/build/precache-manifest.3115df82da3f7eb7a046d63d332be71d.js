3f3787282882b3db240f0a10c0aeea316b8f0f4e
Lohan Batista <lohan_vinicius@hotmail.com>
2021-01-06 14:49:07 -0300

    aula 6

---
+++ 6 - React - Automatizando os testes em aplicações front-end/build/precache-manifest.3115df82da3f7eb7a046d63d332be71d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d09f5df4306547e2dcfe286c4d3138ab",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9a8c972081d0f6ac957c",
+    "url": "/static/css/main.bd8adda8.chunk.css"
+  },
+  {
+    "revision": "c4f724ec0f46824624bb",
+    "url": "/static/js/2.6660fae9.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.6660fae9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a8c972081d0f6ac957c",
+    "url": "/static/js/main.b41c8e6e.chunk.js"
+  },
+  {
+    "revision": "c0c549c145d80a57388a",
+    "url": "/static/js/runtime-main.08f78479.js"
+  }
+]);
